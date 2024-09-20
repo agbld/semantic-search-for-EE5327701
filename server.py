eb5adca84d6341098b2b587737720b22fa55aba1
agbld <agiblida@gmail.com>
2024-09-20 23:52:03 +0800

    add model selection for server.py

--- server.py
+++ server.py
@@ -1,6 +1,17 @@
-from ecombert import get_ecombert, inference
+import argparse
 
-model, tokenizer = get_ecombert()
+parser = argparse.ArgumentParser()
+parser.add_argument("--model_path", type=str, default="./save_model/ABRSS_student_L3_onnx_QINT8")
+parser.add_argument("--model_type", type=str, default="ecombert", choices=["ecombert", "ckipbert"])
+parser.add_argument("--port", type=int, default=5000)
+args = parser.parse_args()
+
+if args.model_type == "ecombert":
+    from ecombert import get_ecombert, inference
+    model, tokenizer = get_ecombert(args.model_path)
+elif args.model_type == "ckipbert":
+    from ckipbert import get_ckipbert, inference
+    model, tokenizer = get_ckipbert(args.model_path)
 
 from flask import Flask, request
 from concurrent.futures import ThreadPoolExecutor
